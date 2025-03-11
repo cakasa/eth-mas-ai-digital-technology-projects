cb9f4b6f9660cd4c1260c118a5a478eb2ce4f05b
Bozhidar Andonov <bandonov@student.ethz.ch>
2025-03-11 11:11:13 +0000

    Add first streamlit fields

--- loan-default/streamlit_app.py
+++ loan-default/streamlit_app.py
@@ -7,17 +7,13 @@ path = './work/eth-mas-ai-digital-technology-projects/loan-default'
 model = joblib.load(path + '/model/logistic_regression_model.pkl')
 imputer_dict = joblib.load(path + '/model/imputer.pkl')
 scaler = joblib.load(path + '/model/scaler.pkl')
-print(path + '/model/logistic_regression_model.pkl')
-print(model, imputer_dict, scaler)
 
-st.title("Hello Streamlit-er")
-columns = st.columns(2)
+loan_request_form = st.form('loan request')
 
-for column in columns:
-    with column:
-        st.markdown('I am a column.')
-
-
+loan_amount = loan_request_form.number_input('Loan Amount', min_value=100, max_value=100_000, value=1000, step=100)
+annual_income = loan_request_form.number_input('Annual Income', min_value=0, value=12000, step=100)
+if loan_request_form.form_submit_button('Request loan')
+    st.markdown("Hello Streamlit-er")
 
 if st.button("Send balloons!"):
     st.balloons()
