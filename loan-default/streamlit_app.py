feffd222219ea65ea57df11d4d8df7e02c601338
Bozhidar Andonov <bandonov@student.ethz.ch>
2025-03-11 11:13:50 +0000

    Fix syntax error

--- loan-default/streamlit_app.py
+++ loan-default/streamlit_app.py
@@ -12,7 +12,7 @@ loan_request_form = st.form('loan request')
 
 loan_amount = loan_request_form.number_input('Loan Amount', min_value=100, max_value=100_000, value=1000, step=100)
 annual_income = loan_request_form.number_input('Annual Income', min_value=0, value=12000, step=100)
-if loan_request_form.form_submit_button('Request loan')
+if loan_request_form.form_submit_button('Request loan'):
     st.markdown("Hello Streamlit-er")
 
 if st.button("Send balloons!"):
