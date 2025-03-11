f393e498a3ff7cb644c634d7306f47d95f37be3a
Bozhidar Andonov <bandonov@student.ethz.ch>
2025-03-11 10:38:18 +0000

    Find out cwd of streamlit app

--- loan-default/streamlit_app.py
+++ loan-default/streamlit_app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 import joblib
+import os
 
-model = joblib.load('./model/logistic_regression_model.pkl')
-imputer_dict = joblib.load('./model/imputer.pkl')
-scaler = joblib.load('./model/scaler.pkl')
-print(model, imputer_dict, scaler)
+st.title(os.getcwd())
+
+# model = joblib.load('./model/logistic_regression_model.pkl')
+# imputer_dict = joblib.load('./model/imputer.pkl')
+# scaler = joblib.load('./model/scaler.pkl')
+# print(model, imputer_dict, scaler)
 
 st.title("Hello Streamlit-er")
 columns = st.columns(2)
