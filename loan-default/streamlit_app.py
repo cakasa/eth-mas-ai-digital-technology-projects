2c60431c9019a8117906c3582d05327264f262dd
Bozhidar Andonov <bandonov@student.ethz.ch>
2025-03-11 11:36:57 +0000

    Add duration and installment fields to streamlit app

--- loan-default/streamlit_app.py
+++ loan-default/streamlit_app.py
@@ -2,19 +2,28 @@ import streamlit as st
 import joblib
 import os
 
-def request_loan():
-    return 1, 0.8
-
 path = './work/eth-mas-ai-digital-technology-projects/loan-default'
 
 model = joblib.load(path + '/model/logistic_regression_model.pkl')
 imputer_dict = joblib.load(path + '/model/imputer.pkl')
 scaler = joblib.load(path + '/model/scaler.pkl')
+def request_loan():
+    return 0.8
 
 loan_request_form = st.form('loan request')
 
 loan_amount = loan_request_form.number_input('Loan Amount', min_value=100, max_value=100_000, value=1000, step=100)
 annual_income = loan_request_form.number_input('Annual Income', min_value=0, value=12000, step=100)
+
+duration = lst.radio(
+    'Loan Duration',
+    [36, 60],
+    format_func=lambda duration: f'{duration} months'
+)
+
+installment = round(loan_amount / duration, 2)
+loan_request_form.markdown(f'Per month: {installment}')
+
 if loan_request_form.form_submit_button('Request loan'):
     probability_granted = request_loan()
     confidence = 0
